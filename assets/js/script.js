d4705d199a641d2ca0bba7a1448e36c628b35f65
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-17 15:47:25 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+/*
+--- Brainstorming ---
+
+Task:
+
+1. Chiedi all’utente il suo nome;
+
+2. Chiedi il suo cognome;
+
+3. Chiedi il suo colore preferito;
+
+4. Scrivi sulla pagina nomecognomecolorepreferito23.
+
+Steps da fare:
+
+1. Salvare il nome inserito dall'utente  in una variabile;
+
+2. Salvare il cognome inserito dall'utente in una variabile;
+
+3. Salvare il colore preferito inserito dall'utente in una variabile;
+
+4. Sommare le tre variabili tra loro;
+
+5. Stampare a schermo il risultato.
+
+*/
